d8bc4c313da3046bc1612e65f1423904863445b9
GitHub Action <action@github.com>
2025-01-20 10:46:22 +0000

    Initialize template repository proper
    Set package name to com.github.codeflusher.adaptivezoom
    Set project name to AdaptiveZoom
    Set modid to adaptivezoom

--- src/main/java/com/github/codeflusher/adaptivezoom/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/codeflusher/adaptivezoom/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.codeflusher.adaptivezoom.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
